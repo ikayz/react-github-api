9eaccfc35e5a2c883a0f240ed9d3a5d45a4eac6c
Isaac Miti <ikayz@live.com>
2018-12-21 10:58:51 +0200

    Added token via config.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import ApolloClient from "apollo-client";
 import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-
+import { TOKEN } from './config';
 
 const httpLink = createHttpLink({
   uri: 'https://api.github.com/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = process.env.REACT_APP_TOKEN
+  const token = TOKEN;
   return {
     headers: {
       ...headers,
